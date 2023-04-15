7d72b48fb2a4500740648d876fed56b2163894f4
Jason Huang <jason97521@gmail.com>
2023-04-15 15:03:29 -0400

    google auth with db

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
         {currentUser ? (
           <User>
             <VideoCallOutlined />
-            <Avatar />
+            <Avatar src={currentUser.img}/>
             {currentUser.name}
           </User>
         ) : <Link to="signin" style={{ textDecoration: "none" }}>
